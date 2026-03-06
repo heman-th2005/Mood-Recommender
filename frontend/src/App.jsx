f0785e142eacbcc43f011bd84a8dd00e119a51e8
HEMANTH R <154581207+heman-th2005@users.noreply.github.com>
2026-03-06 16:05:43 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,7 @@ function App(){
     const[video,setVideo]=useState([]);
 
     const getVideo=async()=>{
-    const res=await axios.post("http://localhost:5001/mood",
+    const res=await axios.post("https://mood-recommender-wzwe.onrender.com/mood",
     {text:text});
     setVideo(res.data);
     };
